1dfe2c3a7b0b08f00e81568f1ab5f5815be700f4
Daniel Watts <d.watts15@outlook.com>
2022-02-16 12:22:56 +0000

    Inluded dbnames in sql files

--- db/login/01_create_login_table.sql
+++ db/login/01_create_login_table.sql
@@ -1,1 +1,2 @@
+-- DATABASE NAME: login
 CREATE TABLE login_details(id SERIAL PRIMARY KEY, username VARCHAR(20), password VARCHAR(40));
--- db/properties/01_create_spaces_table.sql
+++ db/properties/01_create_spaces_table.sql
@@ -1,1 +1,2 @@
+-- Database name: makersbnb
 CREATE TABLE spaces(id SERIAL PRIMARY KEY, name VARCHAR(20), price VARCHAR(20), availability VARCHAR(20), description VARCHAR(1000));
